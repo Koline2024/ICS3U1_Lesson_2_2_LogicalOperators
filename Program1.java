c2a11954ec35d1c2e9411116ac9dd329f685d45e
Eric Fabroa <eric.fabroa@ycdsbk12.ca>
2024-02-26 11:48:52 -0500

    Update Program1.java

--- Program1.java
+++ Program1.java
@@ -1,13 +1,20 @@
 class Program1 extends ConsoleProgram {
 
   /**
-  * Description
+  * Demonstration of logical operators `&&`, `||`, `!`
   * @author:
   */
   
   public void run() {
-    // your code goes here
     
+    // start coding here
+    int a = 5;
+    System.out.println(((1 < a) && (a < 10)));
+    System.out.println((1 < a) && (a < 4));
+
+    System.out.println((a == 4) || (a == 5));
+    System.out.println((a > 2) || (a < 10));
+    System.out.println ((a >= 6) || (a <=4));
     
   }
 }
