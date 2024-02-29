bb9244fe80a663b5ad68e9602d4f4d7a334517c9
koline2024 <christopherxia2020@gmail.com>
2024-02-28 22:05:11 -0500

    A

--- Program1.java
+++ Program1.java
@@ -8,15 +8,35 @@ class Program1 extends ConsoleProgram {
   
   public void run() {
     
-    Integer intEntered = readInt("Enter the PIN. ");
-    Integer intPassword = 1234;
+    int intEntered = readInt("Enter the PIN. ");
+    int intPassword = 12345;
     Boolean blnPasswordTrue = (intEntered == intPassword);
     if (blnPasswordTrue == true) {
 
-      System.out.println("Nice");
+      System.out.println("Access Granted. What would you like to calculate? ");
       
+      double dblOne = readDouble("Enter number one: ");
+      double dblTwo = readDouble ("Enter number two: ");
+      double dblThree = readDouble ("Enter number three: ");
+      double dblFour = readDouble ("Enter number four:");
+
+      double dblSum = dblOne + dblTwo + dblThree + dblFour;
+      double dblDifference = dblOne - dblTwo - dblThree - dblFour;
+      double dblProduct = dblOne * dblTwo *  dblThree * dblFour;
+      double dblDivide = dblOne / dblTwo / dblThree / dblFour;
+      double dblAverage = (dblOne + dblTwo + dblThree + dblFour)/4;
+
+
+      System.out.println("Sum is " + dblSum);
+      System.out.println("Difference is " + dblDifference);
+      System.out.println("Product is " + dblProduct);
+      System.out.println("Division is " + dblDivide);
+      System.out.println("Average is " + dblAverage);
+      
+
+
     } else {
-      System.out.println("Bruh mistake");
+      System.out.println("Wrong!");
     }
     
   }
